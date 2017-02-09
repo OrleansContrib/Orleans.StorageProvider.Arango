49a74e0bd093750cdf57a5e6dabf0ca88e04f2ec
Richard Astbury <richard.astbury@gmail.com>
2017-02-09 10:30:07 +0000

    support exception serialization

--- Orleans.StorageProvider.Arango/ArangoStorageException.cs
+++ Orleans.StorageProvider.Arango/ArangoStorageException.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,13 @@ namespace Orleans.StorageProvider.Arango
     [Serializable]
     public class ArangoStorageException : Exception
     {
-        public ArangoStorageException()
+        public ArangoStorageException() : base ()
         { }
 
         public ArangoStorageException(string message) : base(message) { }
-      
+
+        public ArangoStorageException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+
     }
 }
